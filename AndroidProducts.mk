1172f1b2ee3864fef66c6746d5212ceef9560632
ripee <ripee.dev@gmail.com>
2021-01-08 20:45:24 +0100

    tbelteskt: Move RR to its own branch

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,8 +14,7 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_tbelteskt.mk \
-    $(LOCAL_DIR)/rr_tbelteskt.mk
+    $(LOCAL_DIR)/lineage_tbelteskt.mk
 
 COMMON_LUNCH_CHOICES := \
     lineage_tbelteskt-user \
