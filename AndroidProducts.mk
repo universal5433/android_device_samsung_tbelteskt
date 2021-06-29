e0185d0edbeda568a20d499ba69246d970e0c2a5
Anan Jaser <ananjaser@gmail.com>
2021-06-29 06:50:59 +0400

    tbelteskt: RR-ify

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2018 The LineageOS Project
+# Copyright (C) 2021 The Ressurection Remix Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_tbelteskt.mk
+    $(LOCAL_DIR)/rr_tbelteskt.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_tbelteskt-user \
-    lineage_tbelteskt-userdebug \
-    lineage_tbelteskt-eng
+    rr_tbelteskt-user \
+    rr_tbelteskt-userdebug \
+    rr_tbelteskt-eng
